6b82b2954104a120663d02f069738e219c599673
Andrew Ho <andrewho@andrewho.co.uk>
2009-06-14 10:43:07 +0000

    Clean install of CakePHP 1.2.3.8166 (latest stable)

---
+++ schema.sql
@@ -0,0 +1,29 @@
+--
+-- PostgreSQL database dump
+--
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+DROP SCHEMA public;
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA public;
+
+
+--
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON SCHEMA public IS 'standard public schema';
+
+
+--
+-- PostgreSQL database dump complete
+--
+
