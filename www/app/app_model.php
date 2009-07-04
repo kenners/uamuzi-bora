b43ff2a4dc1f9be4cbfb13ed2deff4b5625c4368
Andrew Ho <andrewho@andrewho.co.uk>
2009-07-04 10:14:18 +0100

    Abstracted code for lookup tables into LookupTableModel (which extends AppModel) so that, for example, validation code doesn't require re-writing as all the lookup tables have the same schema.
    Therefore, when creating models for lookup tables, define them as 'extends
    LookupTableModel'.

--- www/app/app_model.php
+++ www/app/app_model.php
@@ -18,4 +18,44 @@ class AppModel extends Model {
 		return $count == 0;
 	}
 }
+
+/**
+ * Model for lookup tables, so create models like so:
+ *     class Something extends LookupTableModel {...
+ */
+class LookupTableModel extends AppModel {
+	/**
+	 * Validation suite (essentially this is what is checked when save() is
+	 * is called)
+	 */
+	var $validate = array(
+		'id' => array(
+			'int' => array(
+				'rule' => array('decimal', 0),
+				'allowEmpty' => TRUE,
+				'message' => 'The ID should be an integer'
+			),
+			'positive' => array(
+				'rule' => array('comparison', 'greater or equal', 0),
+				'allowEmpty' => TRUE,
+				'message' => 'The ID should be positive'
+			),
+			'unique' => array(
+				'rule' => 'isUnique',
+				'allowEmpty' => TRUE,
+				'message' => 'This ID is already in use'
+			)
+		),
+		'name' => array(
+			'not null' => array(
+				'rule' => 'notEmpty',
+				'message' => 'This field must not be left empty'
+			),
+			'unique' => array(
+				'rule' => array('isUniqueCaseInsensitive', 'name'),
+				'message' => 'This occupation already exists'
+			)
+		)
+	);
+}
 ?>
--- www/app/models/occupation.php
+++ www/app/models/occupation.php
@@ -3,41 +3,7 @@
  * The Occupation model, which describes the lookup table of occupations.
  */
 
-class Occupation extends AppModel {
+class Occupation extends LookupTableModel {
 	var $name = 'Occupation';
-	
-	/**
-	 * Validation suite (essentially this is what is checked when save() is
-	 * is called)
-	 */
-	var $validate = array(
-		'id' => array(
-			'int' => array(
-				'rule' => array('decimal', 0),
-				'allowEmpty' => TRUE,
-				'message' => 'The ID should be an integer'
-			),
-			'positive' => array(
-				'rule' => array('comparison', 'greater or equal', 0),
-				'allowEmpty' => TRUE,
-				'message' => 'The ID should be positive'
-			),
-			'unique' => array(
-				'rule' => 'isUnique',
-				'allowEmpty' => TRUE,
-				'message' => 'This ID is already in use'
-			)
-		),
-		'name' => array(
-			'not null' => array(
-				'rule' => 'notEmpty',
-				'message' => 'This field must not be left empty'
-			),
-			'unique' => array(
-				'rule' => array('isUniqueCaseInsensitive', 'name'),
-				'message' => 'This occupation already exists'
-			)
-		)
-	);
 }
 ?>
