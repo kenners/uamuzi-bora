c5e2b4316f2afde3d2220d2ac6ddd9aa06be5b3d
Kenrick Turner <kenrickturner@gmail.com>
2009-07-10 23:03:16 +0100

    Adding Jambo controller. This will generate the welcome screen according to the logged in user's privileges and serve as the main 'home' screen.

---
+++ www/app/controllers/jambo_controller.php
@@ -0,0 +1,10 @@
+<?php
+class JamboController extends AppController {
+/**
+ * Jambo (Swahili for "hello") is the controller for generating the welcome/home screen views
+ * according to the privileges of the logged in user.
+ */
+
+
+}
+?>
