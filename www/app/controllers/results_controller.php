a4d93200974410e12ee58acbc9658851bc93ab7c
Kenrick Turner <kenrickturner@gmail.com>
2009-07-14 02:58:18 +0100

    Edited and refactored Results::Add method.

--- www/app/controllers/results_controller.php
+++ www/app/controllers/results_controller.php
@@ -21,45 +21,50 @@ class ResultsController extends AppController {
 	}
 
 	function add($pid = null) {
-
-	 
-	  $this->Result->Patient->id=$pid;
-	  $this->set('pid',$pid);
-	  if(!$this->Result->Patient->exists()){
-	    $this->Session->setFlash('You tried to add a result to a Patient that does not exist');
-	    $this->redirect($this->referer());
-	  }
-	  
-	  if (!empty($this->data)) {
-	    $test_id=$this->data['id'];
-	  	$this->Result->Test->id=$test_id;
-	 	//var_dump($this->data);
-	    if($test_id!=0)
-	      {
-		if($this->Result->Test->exists())
-		  {
-		    $this->set('test_id',$test_id);
-		    $this->set('type',$this->Result->Test->find('first',array('conditions'=>array('Test.id'=>$test_id),'recursive'=>-1)));
-		  }else{
-		    $this->Session->setFlash('Not a valid test');
-		  }
-	      }else{
-
-		$this->Result->create();
-		$this->data=Set::insert($this->data,'Result.user_id',$this->Auth->user('id'));
-		$this->data=Set::insert($this->data,'Result.pid',$pid);
-			
-			
-		if ($this->Result->save($this->data)) {
-		  $this->Session->setFlash(__('The Result has been saved', true));
-		  $this->redirect(array('action'=>'index'));
-		} else {
-		  $this->Session->setFlash(__('The Result could not be saved. Please, try again.', true));
+		$this->Result->Patient->id=$pid;
+		$this->set('pid',$pid);
+		if(!$this->Result->Patient->exists()) {
+			$this->Session->setFlash('You tried to add a result to a Patient that does not exist');
+			$this->redirect($this->referer());
 		}
-	      }
-	  }
-	  $tests = $this->Result->Test->find('list');
-	  $this->set(compact('tests'));
+		// Check that we are dealing with data from a form, either from the miniform in the Patients::View view or
+		// from an earlier submission of this controller's form.
+		if (!empty($this->data)) {
+			// But which form are we coming from?
+			if (array_key_exists('Result', $this->data)) {
+				// So this method has been called by the view, with the new result form data having been submitted.
+				// Let's save it.
+				$this->Result->create();
+				$this->data=Set::insert($this->data,'Result.user_id',$this->Auth->user('id'));
+				$this->data=Set::insert($this->data,'Result.pid',$pid);
+				if ($this->Result->save($this->data)) {
+					$this->Session->setFlash(__('The Result has been saved', true));
+					$this->redirect(array('controller'=>'patients','action'=>'view/'.$pid));
+				} else {
+					$this->Session->setFlash(__('The Result could not be saved. Please, try again.', true));
+				}  
+			} else {
+				// So we're coming from the miniform
+				$test_id=$this->data['id'];
+				$this->Result->Test->id=$test_id;
+				// Let's check to see if the Test ID submitted via the miniform actually exists as a Test
+				if($this->Result->Test->exists()) {
+					// Yes, it appears that the Test does exist
+					$this->set('test_id',$test_id);
+					// Let's find out what Type (decimal,lookup,text etc) of test it is
+					$this->set('type',$this->Result->Test->find('first',array('conditions'=>array('Test.id'=>$test_id),'recursive'=>-1)));
+				
+					// Now get the data to send to the view to build the add results form
+					$tests = $this->Result->Test->find('list');
+					$this->set(compact('tests'));
+				} else {
+					// Nope, the Test ID is not valid.
+					$this->Session->setFlash('Not a valid test');
+					// Go back to whence ye came...
+					$this->redirect($this->referer());
+				}
+			}
+		}
 	}
 
 	function edit($id = null) {
