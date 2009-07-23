94b0ac944516b93cc340a04978747565b42c828e
Kenrick Turner <kenrickturner@gmail.com>
2009-07-24 00:03:34 +0300

    Updated tests.sql with feedback from clinic staff.

--- tests.sql
+++ tests.sql
@@ -4,48 +4,94 @@
 
 COPY result_lookups (id, test_id, value, description, comment, user_id, modified) FROM stdin;
 1	1	A	Arrived		1	2009-07-19 22:38:09
-2	1	BS	Being Seen		1	2009-07-19 22:38:21
-3	1	S	Seen		1	2009-07-19 22:38:59
-4	5	N	No		1	2009-07-19 22:41:43
-5	5	Y	Yes		1	2009-07-19 22:42:04
-6	7	Y	Yes		1	2009-07-19 22:44:48
-7	7	N	No		1	2009-07-19 22:44:58
-8	8	1	No Signs		1	2009-07-19 22:46:21
-9	8	2	TB Suspected		1	2009-07-19 22:46:31
-10	8	3	Receiving TB Treatment		1	2009-07-19 22:46:45
-11	9	1	Herpes Zoster		1	2009-07-19 22:48:32
-12	9	2	Pneumonia		1	2009-07-19 22:48:41
-13	9	3	Dementia/Encephalitis		1	2009-07-19 22:48:55
-14	9	4	Thrush - Oral/Vaginal		1	2009-07-19 22:49:36
-15	9	5	Ulcers - Mouth, etc.		1	2009-07-19 22:51:10
-16	9	6	Fever		1	2009-07-19 22:51:20
-17	9	7	Cough		1	2009-07-19 22:51:28
-18	9	8	Difficulty Breathing		1	2009-07-19 22:51:39
-19	9	9	IRIS (Immune Reconstitution Inflammatory Syndrome)		1	2009-07-19 22:52:00
-20	9	10	Weight Loss		1	2009-07-19 22:52:10
-21	9	11	Urethral Discharge		1	2009-07-19 22:52:19
-22	9	12	PID (Pelvic Inflammatory Disease)		1	2009-07-19 22:52:32
-23	9	13	GUD (Genital Ulcerative Disease)		1	2009-07-19 22:52:48
-24	10	1	Peripheral Neuropathy		1	2009-07-19 22:53:46
-25	10	2	Rash		1	2009-07-19 22:53:53
-26	10	3	Anaemia		1	2009-07-19 22:54:00
-27	10	4	Pancreatitis		1	2009-07-19 22:54:09
-28	10	5	Jaundice		1	2009-07-19 22:54:19
-29	10	6	Fat Redistribution		1	2009-07-19 22:54:31
-30	10	7	Hypersensitivity		1	2009-07-19 22:54:41
-31	10	8	Hepatotoxicity		1	2009-07-19 22:54:51
-32	10	9	CNS Symptoms (Dizziness, Anxiety, Depression)		1	2009-07-19 22:55:10
-33	11	YS	Yes (Satisfactory Adherence)		1	2009-07-19 22:56:30
-34	11	YU	Yes (Unsatisfactory Adherence)		1	2009-07-19 22:56:46
-35	11	N	No		1	2009-07-19 22:56:54
-36	12	YS	Yes (Satisfactory Adherence)		1	2009-07-19 22:57:34
-37	12	YU	Yes (Unsatisfactory Adherence)		1	2009-07-19 22:57:46
-38	12	N	No		1	2009-07-19 22:58:04
-39	14	0	Unknown		1	2009-07-19 22:59:52
-40	15	YS	Yes (Satisfactory Adherence)		1	2009-07-19 23:00:35
-41	15	YU	Yes (Unsatisfactory Adherence)		1	2009-07-19 23:00:44
-42	15	N	No		1	2009-07-19 23:00:52
-43	20	0	Unknown		1	2009-07-19 23:04:20
+2	1	S	Seen		1	2009-07-19 22:38:59
+3	6	1	Stage 1		1	2009-07-19 22:38:59
+4	6	2	Stage 2		1	2009-07-19 22:38:59
+5	6	3	Stage 3		1	2009-07-19 22:38:59
+6	6	4	Stage 4		1	2009-07-19 22:38:59
+7	7	N	No		1	2009-07-19 22:41:43
+8	7	Y	Yes		1	2009-07-19 22:42:04
+9	9	Y	Yes		1	2009-07-19 22:44:48
+10	9	N	No		1	2009-07-19 22:44:58
+11	10	1	No Signs		1	2009-07-19 22:46:21
+12	10	2	TB Suspected		1	2009-07-19 22:46:31
+13	10	3	Receiving TB Treatment		1	2009-07-19 22:46:45
+14	11	1	Herpes Zoster		1	2009-07-19 22:48:32
+15	11	2	Pneumonia		1	2009-07-19 22:48:41
+16	11	3	Dementia/Encephalitis		1	2009-07-19 22:48:55
+17	11	4	Thrush - Oral/Vaginal		1	2009-07-19 22:49:36
+18	11	5	Ulcers - Mouth, etc.		1	2009-07-19 22:51:10
+19	11	6	Fever		1	2009-07-19 22:51:20
+20	11	7	Cough		1	2009-07-19 22:51:28
+21	11	8	Difficulty Breathing		1	2009-07-19 22:51:39
+22	11	9	IRIS (Immune Reconstitution Inflammatory Syndrome)		1	2009-07-19 22:52:00
+23	11	10	Weight Loss		1	2009-07-19 22:52:10
+24	11	11	Urethral Discharge		1	2009-07-19 22:52:19
+25	11	12	PID (Pelvic Inflammatory Disease)		1	2009-07-19 22:52:32
+26	11	13	GUD (Genital Ulcerative Disease)		1	2009-07-19 22:52:48
+27	13	1	Peripheral Neuropathy		1	2009-07-19 22:53:46
+28	13	2	Rash		1	2009-07-19 22:53:53
+29	13	3	Anaemia		1	2009-07-19 22:54:00
+30	13	4	Pancreatitis		1	2009-07-19 22:54:09
+31	13	5	Jaundice		1	2009-07-19 22:54:19
+32	13	6	Fat Redistribution		1	2009-07-19 22:54:31
+33	13	7	Hypersensitivity		1	2009-07-19 22:54:41
+34	13	8	Hepatotoxicity		1	2009-07-19 22:54:51
+35	13	9	CNS Symptoms (Dizziness, Anxiety, Depression)		1	2009-07-19 22:55:10
+36	14	Y	Yes		1	2009-07-19 22:56:30
+37	14	YS	Yes (Satisfactory Adherence)		1	2009-07-19 22:56:30
+38	14	YU	Yes (Unsatisfactory Adherence)		1	2009-07-19 22:56:46
+39	14	N	No		1	2009-07-19 22:56:54
+40	15	Y	Yes		1	2009-07-19 22:56:30
+41	15	YS	Yes (Satisfactory Adherence)		1	2009-07-19 22:57:34
+42	15	YU	Yes (Unsatisfactory Adherence)		1	2009-07-19 22:57:46
+43	15	N	No		1	2009-07-19 22:58:04
+44	17	1A	1A (D4T30/3TC/NVP)	1st Line	1	2009-07-19 22:38:09
+45	17	2A	2A (D4T30/3TC/EFV)	1st Line	1	2009-07-19 22:38:09
+46	17	3A	3A (AZT/3TC/EFV)	1st Line	1	2009-07-19 22:38:09
+47	17	3B	3B (AZT/3TC/NVP)	1st Line	1	2009-07-19 22:38:09
+48	17	R66	R66 (TDF/3FC/NVP)	2nd Line	1	2009-07-19 22:38:09
+49	17	R67	R67 (ABC/DDI/LPVR)	2nd Line	1	2009-07-19 22:38:09
+50	17	PEP1	PEP1 (AZT/3TC)	PEP	1	2009-07-19 22:38:09
+51	17	O	Other	Other	1	2009-07-19 22:38:09
+52	17	U	Unknown	Unknown	1	2009-07-19 22:38:09
+53	18	Y	Yes		1	2009-07-19 22:56:30
+54	18	YS	Yes (Satisfactory Adherence)		1	2009-07-19 23:00:35
+55	18	YU	Yes (Unsatisfactory Adherence)		1	2009-07-19 23:00:44
+56	18	N	No		1	2009-07-19 23:00:52
+57	25	1	Basic HIV Education, transmission		1	2009-07-19 23:04:20
+58	25	2	Prevention: abstinenece, safer sex, condoms		1	2009-07-19 23:04:20
+59	25	3	Prevention: household precautions, what is safe		1	2009-07-19 23:04:20
+60	25	4	Post-test counselling: implications of results		1	2009-07-19 23:04:20
+61	25	5	Positive living		1	2009-07-19 23:04:20
+62	25	6	Testing partners-Discordants		1	2009-07-19 23:04:20
+63	25	7	Disclosure		1	2009-07-19 23:04:20
+64	25	8	To whom disclosed		1	2009-07-19 23:04:20
+65	25	9	Family/living situation		1	2009-07-19 23:04:20
+66	25	10	Shared confidentiality		1	2009-07-19 23:04:20
+67	25	11	Reproductive choices, prevention MTCT		1	2009-07-19 23:04:20
+68	25	12	Progression of disease		1	2009-07-19 23:04:20
+69	25	13	Available treatment/prophylaxis		1	2009-07-19 23:04:20
+70	25	14	Follow-up appointments, clinical team		1	2009-07-19 23:04:20
+71	25	15	CTX, prophylaxis		1	2009-07-19 23:04:20
+72	25	16	ART - educate on essentials (locally adapted)		1	2009-07-19 23:04:20
+73	25	17	Why complete adherence needed		1	2009-07-19 23:04:20
+74	25	18	Adherence preparation		1	2009-07-19 23:04:20
+75	25	19	Indicate when READY for ART		1	2009-07-19 23:04:20
+76	25	20	Explain dose, when to take		1	2009-07-19 23:04:20
+77	25	21	What can occur, how to manage SEs		1	2009-07-19 23:04:20
+78	25	22	What to do if one forgets dose		1	2009-07-19 23:04:20
+79	25	23	What to do when travelling		1	2009-07-19 23:04:20
+80	25	24	Adherence plan (schedule, aids, explain diary)		1	2009-07-19 23:04:20
+81	25	25	Treatment supporter preparation		1	2009-07-19 23:04:20
+82	25	26	Which doses, why missed		1	2009-07-19 23:04:20
+83	25	27	ARV support group		1	2009-07-19 23:04:20
+84	25	28	How to contact clinic		1	2009-07-19 23:04:20
+85	25	29	Symptom management/palliative care at home		1	2009-07-19 23:04:20
+86	25	30	Caregiver booklet		1	2009-07-19 23:04:20
+87	25	31	Home-based care		1	2009-07-19 23:04:20
+88	25	32	Support groups		1	2009-07-19 23:04:20
+89	25	33	Community support		1	2009-07-19 23:04:20
 \.
 
 --
@@ -57,23 +103,27 @@ COPY tests (id, name, abbreiviation, type, upper_limit, lower_limit, description
 2	Weight	Wt	decimal	\N	\N	The patient's current weight in kilograms.		t	1	2009-07-19 22:39:59	kg
 3	Height	Ht	decimal	\N	\N	The patient's current height in metres.		t	1	2009-07-19 22:40:22	m
 4	Temperature	Temp	decimal	\N	\N	The patient's temperature in degrees Celsius.		t	1	2009-07-19 22:41:10	ºC
-5	Pregnant		lookup	\N	\N	Is the patient currently pregnant?		t	1	2009-07-19 22:41:32	
-6	Last Menstrual Period	LMP	text	\N	\N	Date of the patient's last menstrual period in the format DD-MM-YYYY		t	1	2009-07-19 22:43:46	
-7	Family Planning Status		lookup	\N	\N	Is the patient using any form of contraception?		t	1	2009-07-19 22:44:36	
-8	TB Status		lookup	\N	\N	The current TB status of the patient.		t	1	2009-07-19 22:46:10	
-9	New Opportunistic Infection or Other Medical Conditions	NewOI	lookup	\N	\N	Does the patient have a new opportunistic infection or other medical condition?		t	1	2009-07-19 22:48:23	
-10	ART Side Effects	ART SE	lookup	\N	\N	Is the patient currently experiencing any side-effects of ARTs?		t	1	2009-07-19 22:53:32	
-11	Cotrimoxazole Status		lookup	\N	\N	Is the patient taking cotrimoxazole?		t	1	2009-07-19 22:56:15	
-12	Fluconazole Status		lookup	\N	\N	Is the patient taking fluconazole?		t	1	2009-07-19 22:57:27	
-13	Other Medications Dispensed		text	\N	\N	Have any other medications been dispensed to the patient?		t	1	2009-07-19 22:58:40	
-14	ARV Drug Regimen		lookup	\N	\N			f	1	2009-07-19 22:59:43	
-15	ARV Drug Status		lookup	\N	\N			t	1	2009-07-19 23:00:28	
-16	CD4 Count	CD4	decimal	\N	\N	The patient's current CD4 count.		t	1	2009-07-19 23:01:45	%
-17	Haemaglobin	Hb	decimal	\N	\N	The patient's current haemaglobin level.		t	1	2009-07-19 23:02:16	g/dL
-18	White Cell Count	WCC	decimal	\N	\N	The patient's current white cell count.		t	1	2009-07-19 23:03:10	x10^9
-19	ALT	ALT	decimal	\N	\N			t	1	2009-07-19 23:03:50	IU
-20	Referred To		lookup	\N	\N			f	1	2009-07-19 23:04:13	
-21	Date of Next Appointment		text	\N	\N	The date of the patient's next appointment in the format DD-MM-YYYY		t	1	2009-07-19 23:05:12	
-22	Clinical Note		text	\N	\N			t	1	2009-07-19 23:05:44	
-23	Examination Findings		text	\N	\N			t	1	2009-07-19 23:05:58	
+5	Blood Pressure	BP	text	\N	\N	The patient's blood pressure (systolic/diastolic)		t	1	2009-07-19 22:41:10	
+6	WHO Stage		lookup	\N	\N	The patient's current WHO Stage (1-4)		t	1	2009-07-19 22:41:10	
+7	Pregnant		lookup	\N	\N	Is the patient currently pregnant?		t	1	2009-07-19 22:41:32	
+8	Last Menstrual Period	LMP	text	\N	\N	Date of the patient's last menstrual period in the format DD-MM-YYYY		t	1	2009-07-19 22:43:46	
+9	Family Planning Status		lookup	\N	\N	Is the patient using any form of contraception?		t	1	2009-07-19 22:44:36	
+10	TB Status		lookup	\N	\N	The current TB status of the patient.		t	1	2009-07-19 22:46:10	
+11	New Opportunistic Infection	NewOI	lookup	\N	\N	Does the patient have a new opportunistic infection?		t	1	2009-07-19 22:48:23	
+12	Other Medical Conditions		text	\N	\N	Does the patient have any new other medical condition?		t	1	2009-07-19 22:48:23	
+13	ART Side Effects	ART SE	lookup	\N	\N	Is the patient currently experiencing any side-effects of ARTs?		t	1	2009-07-19 22:53:32	
+14	Cotrimoxazole Status		lookup	\N	\N	Is the patient taking cotrimoxazole?		t	1	2009-07-19 22:56:15	
+15	Fluconazole Status		lookup	\N	\N	Is the patient taking fluconazole?		t	1	2009-07-19 22:57:27	
+16	Other Medications Dispensed		text	\N	\N	Have any other medications been dispensed to the patient?		t	1	2009-07-19 22:58:40	
+17	ARV Drug Regimen		lookup	\N	\N			f	1	2009-07-19 22:59:43	
+18	ARV Drug Status		lookup	\N	\N			t	1	2009-07-19 23:00:28	
+19	CD4 Count	CD4	decimal	\N	\N	The patient's current CD4 count.		t	1	2009-07-19 23:01:45	%
+20	Haemaglobin	Hb	decimal	\N	\N	The patient's current haemaglobin level.		t	1	2009-07-19 23:02:16	g/dL
+21	White Cell Count	WCC	decimal	\N	\N	The patient's current white cell count.		t	1	2009-07-19 23:03:10	x10^9
+22	ALT	ALT	decimal	\N	\N			t	1	2009-07-19 23:03:50	IU
+23	Referred To		text	\N	\N			f	1	2009-07-19 23:04:13	
+24	Date of Next Appointment		text	\N	\N	The date of the patient's next appointment in the format DD-MM-YYYY		t	1	2009-07-19 23:05:12	
+25	Counselling		lookup	\N	\N	Has the patient received any form of counselling?		t	1	2009-07-19 23:04:24	
+26	Clinical Note		text	\N	\N			t	1	2009-07-19 23:05:44	
+27	Examination Findings		text	\N	\N			t	1	2009-07-19 23:05:58	
 \.
