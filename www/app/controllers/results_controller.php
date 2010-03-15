f974e05ccec9c839a592dbc1a60ec2c11c7048fb
Gulfa <gunnarroe@gmail.com>
2010-03-15 21:45:14 +0000

    Batch add now supports multival. Edit and Add functions in results controller documented.[#69 responsible:none state:resolved]

--- www/app/controllers/results_controller.php
+++ www/app/controllers/results_controller.php
@@ -47,21 +47,19 @@ class ResultsController extends AppController {
 		
 			// But which form are we coming from?
 			if (array_key_exists('Result', $this->data)) {
-			// So this method has been called by the view, with the new result form data having been submitted.
+				// So this method has been called by the view, with the new result form data having been submitted.
 				// need to get the id of the result we are about to add
-
-				
-								// IN case we nedd to reload the form, we set the test_id and type
-								
+		
 
 				//insert pid, user and test id
 				$this->data = Set::insert($this->data, 'Result.user_id', $this->Auth->user('id'));
 				$this->data = Set::insert($this->data, 'Result.pid', $pid);
 				
 				$invalid=array();
-				
+				//find out if the test allows multival to be added
 				$multi = $this->Result->Test->find('first',
-						array('conditions' => array('Test.id' => $this->data['Result']['test_id']), 'recursive' => -1, 'fields' => array('Test.multival')));
+						array('conditions' => array('Test.id' => $this->data['Result']['test_id']),
+									 'recursive' => -1, 'fields' => array('Test.multival')));
 				if($multi['Test']['multival']==TRUE){
 					$i=0;
 					while( $i < count($this->data['ResultValue']['value_lookup']) ){
@@ -77,13 +75,14 @@ class ResultsController extends AppController {
 						$i++;
 					}
 					unset($this->data['ResultValue']['value_lookup']);
-				}else{
+				}else{//Only one value to be added
 					$this->data = Set::insert($this->data, 'ResultValue.user_id', $this->Auth->user('id'));
 				
 					if(!empty($this->data['ResultValue']['value_lookup'])){
 						$this->data = Set::insert($this->data, 'ResultValue.value_lookup', $this->data['ResultValue']['value_lookup'][0]);
 						}
 					$this->Result->ResultValue->set(array('ResultValue'=>$this->data['ResultValue']));
+					//check that the result validates
 					if(!$this->Result->ResultValue->validates()){
 						$invalid[$i]=$this->Result->ResultValue->validationErrors;
 						unset($this->Result->ResultValue->validationErrors);
@@ -101,17 +100,17 @@ class ResultsController extends AppController {
 					//save
 					$this->Result->create();		
 					$this->Result->save($this->data);
+					//get result Id so resultValue can be stored with the correct id
 					$result_id=$this->Result->id;
 
 					if(array_key_exists(0,$this->data['ResultValue'])){//If we have multiple values to store!
 
 						foreach($this->data['ResultValue'] as $val){
 							$val= Set::insert($val,'result_id', $result_id);
-							//debug($val);
-							$this->Result->ResultValue->create();
+							this->Result->ResultValue->create();
 							$this->Result->ResultValue->save(array('ResultValue'=>$val));
 							}
-					}else{
+					}else{//Only one ResultValue to store
 						$this->data = Set::insert($this->data, 'ResultValue.result_id', $result_id);
 
 						$this->Result->ResultValue->create();
@@ -190,25 +189,28 @@ class ResultsController extends AppController {
 			$this->redirect(array('action'=>'index'));
 		}
 		if (!empty($this->data)) {
-				//insert pid, user and test id
-			//debug($this->data);
+			//insert pid, user and test id
 			$this->data = Set::insert($this->data, 'Result.user_id', $this->Auth->user('id'));
-	
+			//find if multival is enabled
 			$multi = $this->Result->Test->find('first',
-				array('conditions' => array('Test.id' => $this->data['Result']['test_id']), 'recursive' => -1, 'fields' => array('Test.multival')));
+				array('conditions' => array('Test.id' => $this->data['Result']['test_id']), 'recursive' => -1, 
+											'fields' => array('Test.multival')));
 			
 			$result_id = $this->data['Result']['id'];
 
 			if($multi['Test']['multival']==TRUE){
 				$i=0;
 				$invalid=array();
-				//debug($this->data);
-				if(!empty($this->data['ResultValue']['value_lookup'])){
-					while($i<count($this->data['ResultValue']['value_lookup'])){
+				if(!empty($this->data['ResultValue']['value_lookup'])){//There are values to be stored(Not all unchecked)
+					while($i<count($this->data['ResultValue']['value_lookup'])){//Loop over all the values we need to add
+					
 						if(!empty($this->data['ResultValue'][$i])){
+							//As long as we have an existing databse record with an id we edit this record
+						// If we run out of ids we will leave that field empty so a new row is added to the result_values table
 							$this->data = Set::insert($this->data, 'ResultValue.'.$i.'.id',$this->data['ResultValue'][$i]['id'] );
 
 						}
+						// Insert data to be stored
 						$this->data = Set::insert($this->data, 'ResultValue.'.$i.'.user_id', $this->Auth->user('id'));
 						$this->data = Set::insert($this->data, 'ResultValue.'.$i.'.result_id', $result_id);
 						$this->data = Set::insert($this->data, 'ResultValue.'.$i.'.value_lookup', $this->data['ResultValue']['value_lookup'][$i]);
@@ -220,6 +222,7 @@ class ResultsController extends AppController {
 						$i++;
 					}
 					while(!empty($this->data['ResultValue'][$i]['id'])){
+						// If we have fewer values than existing ids, we delete the remaing rows in the database table
 						$this->Result->ResultValue->del($this->data['ResultValue'][$i]['id']);
 						unset($this->data['ResultValue'][$i]);
 
@@ -230,7 +233,7 @@ class ResultsController extends AppController {
 				}else{
 					unset($this->data['ResultValue']['value_lookup']);
 
-				//All result values have been deselected so we delete the result.
+					//All result values have been deselected so we delete the result.
 				
 					$this->Result->del($result_id);
 					
@@ -241,7 +244,7 @@ class ResultsController extends AppController {
 					$this->redirect(array('controller' => 'patients', 'action' => 'view/' . $this->data['Result']['pid']));
 				}
 						
-			}else{
+			}else{	//Only one value to edit
 				$this->data = Set::insert($this->data, 'ResultValue.user_id', $this->Auth->user('id'));
 				$this->data = Set::insert($this->data, 'ResultValue.result_id', $result_id);
 				if(!empty($this->data['ResultValue']['value_lookup'])){
@@ -254,8 +257,7 @@ class ResultsController extends AppController {
 					unset($this->Result->ResultValue->validationErrors);
 				}
 			}
-			//debug($this->data);	
-			// validate both inputs
+			
 
 
 			if (empty($invalid)){
@@ -290,6 +292,7 @@ class ResultsController extends AppController {
 
 
 	}
+	// Set up nescesary info for the view
 	$resultValues=$this->Result->ResultValue->find('all',array('conditions'=>array('ResultValue.result_id'=>$id)));
 	$results=$this->Result->read(null,$id);
 	$tests = $this->Result->Test->find('list');
@@ -328,10 +331,11 @@ class ResultsController extends AppController {
 	function batch_add($pid){ 
 		$this->Result->Patient->id = $pid;
 		$this->set('pid', $pid);
-		if(!$this->Result->Patient->exists()) {
+		if(!$this->Result->Patient->exists()) {//Checking that patient exists
 			$this->Session->setFlash('You tried to add results to a Patient that does not exist');
 			$this->redirect($this->referer());
 		}
+		//The tests with the following ids are in the form
 		$batchOfTestIDs = array(2,3,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24);
 			// Loop to pull the relevent info for each of our tests and put them in an array	
 		$batchOfTestInfo = array();
@@ -346,6 +350,7 @@ class ResultsController extends AppController {
 			$testInfo['name']=$t['Test']['name'];	
 			$testInfo['type']=$t['Test']['type'];
 			$testInfo['multival']=$t['Test']['multival'];
+			$testInfo['units']=$t['Test']['units'];
 										
 			// Get Test answers/options if required
 			if($testInfo['type'] == 'lookup') {
@@ -366,13 +371,10 @@ class ResultsController extends AppController {
 		}
 			// Make our fresh array of test info available to the view
 		$this->set('batchOfTests', $batchOfTestInfo);
-		$this->set('Patient',$this->Result->Patient->find('first',array('conditions'=>array('Patient.pid'=>$pid),'recurcive'=>-2,'fields'=>array('pid','surname','forenames'))));
+		$this->set('Patient',$this->Result->Patient->find('first',array('conditions'=>array('Patient.pid'=>$pid),'recurcive'=>-2,'fields'=>array('pid','surname','forenames','upn'))));
 		
 		// Is this a new form i.e. is there data in our object?
-		if (empty($this->data)) {
-		
-		}else {
-			//debug($this->data);
+		if (!empty($this->data)) 
 			$tests=array();
 			$data=$this->data;
 			//Create an array with testid and type
@@ -380,55 +382,46 @@ class ResultsController extends AppController {
 				$t = $this->Result->Test->find('first',array('conditions'=>array('Test.id'=>$test),'recursive'=>-1));
 				$tests[$test]=$t['Test']['type'];
 			}
-			//get dates and Requesting Clinician
+			//get dates 
 			$date=array();
 			$date[]=$data['Result'][0];
 			$date[]=$data['Result'][1];
 			$date[]=$data['Result'][2];
 			$date[]=$data['Result'][3];
-			$date[]=$data['Result'][4];
+			
 			unset($data['Result'][0]);
 			unset($data['Result'][1]);
 			unset($data['Result'][2]);
 			unset($data['Result'][3]);
-			unset($data['Result'][4]);
-			$clin=$data['Result']['requesting_clinician'];
-			unset($data['Result']['requesting_clinician']);
 			
-			$result_id=$this->Result->find('all',array('order'=>' Result.id DESC','recursive'=>0,
-									'fields'=>array('id')));
-			$result_id=$result_id[0]['Result']['id']+1;
+		//	$clin=$data['Result']['requesting_clinician'];
+		//	unset($data['Result']['requesting_clinician']);
+			
+			$result_id=0;
 
 			// loop through all the results and add any fields that have been filled out
-			$counter=5;//Since The dates are the first 4 fields.
+			$counter=4;//Since The dates are the first 4 fields.
+			//Arrays to store the data to be added and the validation errors
 			$invalidResults=array();
 			$invalidResultValues=array();
 			$resultArray=array();
-			$resultValueArray=array();
+			$resultValueArray=array();//This will be an array of arrays
 				
 			foreach($data['ResultValue'] as $result){
-				//find test it by using the fact that there are 5 columns
-				$test_id=$batchOfTestIDs[intval($counter/5) -1];
+				//find test it by using the fact that there are 4 columns
+				$test_id=$batchOfTestIDs[intval($counter/4) -1];
 				//check that we have a value, so that we only add fields that are filled out
 				if(!empty($result['value_'.$tests[$test_id]])){
 					//get the data to add, $counter%  gives us the right date
 					$value=$result['value_'.$tests[$test_id]];
-					$test_id=$batchOfTestIDs[intval($counter/5) -1];
-					$d=$date[$counter % 5];
-					
+					$d=$date[$counter % 4];
+					//This is what we add to the result table
 					$to_addRes=array('Result'=>array('pid'=>$pid,
 									'test_id'=>$test_id,
 									'test_performed'=>$d['test_performed'],
-									'requesting_clinician'=>$clin,
+									//'requesting_clinician'=>$clin,
 									'user_id'=>$this->Auth->user('id')
 									));
-					$to_addResVal=array('ResultValue'=> array(
-										'value_'.$tests[$test_id]=>$value,
-										'user_id'=>$this->Auth->user('id'),
-										'result_id'=>$result_id
-										));
-					$result_id++;
-
 					$this->Result->create();
 					// Set an validate every field, and keep all validation errors
 				        $this->Result->set($to_addRes);
@@ -441,21 +434,48 @@ class ResultsController extends AppController {
 					}
 				
 					$resultArray[]=$to_addRes;
-
+					//Check if multiplevalues have been entered for one result
+					if(!is_array($value)){
+						//Single value
+						$to_addResVal=array('ResultValue'=> array(
+											'value_'.$tests[$test_id]=>$value,
+											'user_id'=>$this->Auth->user('id'),
+											'result_id'=>$result_id
+											));
+				
+				
 
 					
-					// Set an validate every field, and keep all validation errors
-				        $this->Result->ResultValue->set($to_addResVal);
-					if($this->Result->ResultValue->validates()){
+						// Set an validate every field, and keep all validation errors
+				      	  	$this->Result->ResultValue->set($to_addResVal);
+						if($this->Result->ResultValue->validates()){
 						
-						
-					} else {
-						$invalidResultValues[$counter]=$this->Result->ResultValue->validationErrors;
-						unset($this->Result->ResultValue->validationErrors);
-					}
+							
+						} else {
+							$invalidResultValues[$counter]=$this->Result->ResultValue->validationErrors;
+							unset($this->Result->ResultValue->validationErrors);
+						}
 				
-					$resultValueArray[]=$to_addResVal;
+						$resultValueArray[]=array($to_addResVal);
+						
+					}else{
+						//multiple values for one result
+						$one_result=array();
+						
+						//prepare all the resultValues that should be linked to one result
+						foreach($value as $val){
+							$to_addResVal=array('ResultValue'=> array(
+											'value_'.$tests[$test_id]=>$val,
+											'user_id'=>$this->Auth->user('id'),
+											'result_id'=>$result_id
+											));
+							$one_result[]=$to_addResVal;
+							}
+						$resultValueArray[]=$one_result;
+					}
+					
 
+					
 				}
 				
 				$counter++;
@@ -463,17 +483,23 @@ class ResultsController extends AppController {
 			// If no validation problems save
 			
 			if (empty($invalidResults) and empty($invalidResultValues)){
-				foreach($resultArray as $result){
+				$counter=0;
+				// Run through all the results we need to add
+				while ($counter<count($resultValueArray)){
 					$this->Result->create();
-					$this->Result->save($result);
-				}
-				foreach($resultValueArray as $val){
-					$this->Result->ResultValue->create();
-					$this->Result->ResultValue->save($val);
+					$this->Result->save($resultArray[$counter]);
+					// Need the result_id so we can add the ResultValue to the correct result
+					$id=$this->Result->id;
+					foreach ($resultValueArray[$counter] as $value){
+						$this->Result->ResultValue->create();
+						$value = Set::insert($value, 'ResultValue.result_id', $id);
+						$this->Result->ResultValue->save($value);
+					}
+					$counter++;
 				}
 	
 				$this->Session->setFlash(__('The Results have been saved', true));
-				$this->redirect(array('controller'=>'patients','action' => 'index'));	
+				$this->redirect(array('controller'=>'patients','action' => 'view/'.$pid));	
 			}else{//If validation problems disply them.
 			
 				$this->Result->validationErrors = $invalidResults;
